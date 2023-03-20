f50a9ac5d20df2cdf3616963cc58ea0ce6571c49
Austin Wise <AustinWise@gmail.com>
2023-03-19 23:52:38 -0700

    Add more scripts for building and running.

--- examples/SandwichTracker/build.cmd
+++ examples/SandwichTracker/build.cmd
@@ -3,4 +3,22 @@ setlocal
 
 pushd %~dp0
 
-dotnet publish --os linux --arch x64 /t:PublishContainer -c Release
+if "%1" == "" goto wrong_number_of_args
+set __ImageTag=%1
+shift
+if NOT "%1" == "" goto wrong_number_of_args
+
+
+dotnet publish --os linux --arch x64 /t:PublishContainer -c Release -p:ContainerImageTag=%__ImageTag%
+if NOT '%ERRORLEVEL%' == '0' goto exit_with_error
+docker push us-central1-docker.pkg.dev/test-iap-379718/sandwich-apps/sandwichtracker:%__ImageTag%
+if NOT '%ERRORLEVEL%' == '0' goto exit_with_error
+
+exit /b 0
+
+:wrong_number_of_args
+echo Wrong number of arguments, expected exactly one for the image tag name.
+exit /b 1
+
+:exit_with_error
+exit /b 1
