98b072bf3a31f040667ad165218f1fd3963a07c5
Austin Wise <AustinWise@gmail.com>
2026-03-22 16:21:55 -0700

    Fix some missing renames

--- src/AWise.IdentityAwareProxy/Properties/Resources.Designer.cs
+++ src/AWise.IdentityAwareProxy/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace AWise.IdentityAwareProxy.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Austin.IdentityAwareProxy.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AWise.IdentityAwareProxy.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
