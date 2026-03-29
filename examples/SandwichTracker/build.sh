a3189d9b2f1d2e9606409f7ffaea895d03459663
Austin Wise <AustinWise@gmail.com>
2026-03-29 14:03:45 -0700

    Add note about authentication

--- examples/SandwichTracker/build.sh
+++ examples/SandwichTracker/build.sh
@@ -10,4 +10,6 @@ then
 fi
 
 dotnet publish --os linux --arch x64 /t:PublishContainer -c Release -p:ContainerImageTag=$1
+# If this command fails with an "Unauthenticated request" message, run:
+#   gcloud auth configure-docker us-central1-docker.pkg.dev
 docker push us-central1-docker.pkg.dev/test-iap-491718/sandwich-apps/sandwichtracker:$1
